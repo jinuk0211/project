9e60648784f519961874b3ce71ecaea9c4ec9373
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-10-14 03:13:03 +0900

    Update work.ml

--- work.ml
+++ work.ml
@@ -18,26 +18,16 @@ let () =
 
 
 
-(* 2. Max function (recursive version) *)
-let rec max : int list -> int = function
+let rec max_recursive : int list -> int = function
   | [] -> min_int
-  | h :: t -> let max_tail = max t in
-              if h > max_tail then h else max_tail
+  | x :: xs -> max x (max_recursive xs)
 
-(* 3. Max function (using fold_left) *)
 let max_fold : int list -> int = fun lst ->
   match lst with
   | [] -> min_int
-  | h :: t -> List.fold_left (fun acc x -> if x > acc then x else acc) h t
-
-(* Test cases for max functions *)
-let test_max lst =
-  Printf.printf "Input: [%s]\n" (String.concat "; " (List.map string_of_int lst));
-  Printf.printf "max (recursive) = %d\n" (max lst);
-  Printf.printf "max_fold = %d\n\n" (max_fold lst)
+  | x :: xs -> List.fold_left max x xs
 
 
-(* Swapper function *)
 let rec swapper : 'a -> 'a -> 'a list -> 'a list = fun x y l ->
   match l with
   | [] -> []
@@ -47,24 +37,12 @@ let rec swapper : 'a -> 'a -> 'a list -> 'a list = fun x y l ->
       else
         h :: swapper x y t
 
-(* Test cases for swapper function *)
-let test_swapper x y l =
-  Printf.printf "Input: x = %s, y = %s, l = [%s]\n" 
-    (Printf.sprintf "%S" x) 
-    (Printf.sprintf "%S" y) 
-    (String.concat "; " (List.map (Printf.sprintf "%S") l));
-  let result = swapper x y l in
-  Printf.printf "Output: [%s]\n\n" 
-    (String.concat "; " (List.map (Printf.sprintf "%S") result))
-
 let () =
-  test_swapper "apple" "orange" ["apple"; "banana"; "apple"; "cherry"];
-  test_swapper "x" "y" ["x"; "x"; "x"]
+  let result1 = swapper "apple" "orange" ["apple"; "banana"; "apple"; "cherry"] in
+  let result2 = swapper "x" "y" ["x"; "x"; "x"] in
 
 
-(* Previous functions... *)
 
-(* Zipper function *)
 let rec zipper : 'a list -> 'a list -> 'a list = fun a b ->
   match (a, b) with
   | ([], []) -> []
@@ -72,16 +50,7 @@ let rec zipper : 'a list -> 'a list -> 'a list = fun a b ->
   | (as', []) -> as'
   | (x :: xs, y :: ys) -> x :: y :: zipper xs ys
 
-(* Test cases for zipper function *)
-let test_zipper a b =
-  Printf.printf "Input: a = [%s], b = [%s]\n" 
-    (String.concat "; " (List.map string_of_int a))
-    (String.concat "; " (List.map string_of_int b));
-  let result = zipper a b in
-  Printf.printf "Output: [%s]\n\n" 
-    (String.concat "; " (List.map string_of_int result))
-
 let () =
-  test_zipper [1; 2; 4; 5] [6; 7; 9; 10; 11];
-  test_zipper [6; 7; 9; 10; 11] [1; 2; 4; 5];
-  test_zipper [] []
+  let result1 = zipper [1; 2; 4; 5] [6; 7; 9; 10; 11] in
+  let result2 = zipper [6; 7; 9; 10; 11] [1; 2; 4; 5] in
+  let result3 = zipper [] [] in
